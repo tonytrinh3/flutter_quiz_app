7d6cf413e82310ba5168b91118279f393569d63f
tony trinh <tony.w.trinh3@gmail.com>
2020-09-27 14:26:27 -0700

    done reset

--- lib/main.dart
+++ lib/main.dart
@@ -18,21 +18,49 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': "What\s your favorite color?",
-      "answers": ["Black", "Red", "Green", "White"]
+      "answers": [
+        {"text": "Black", "score": 10},
+        {"text": "Red", "score": 5},
+        {"text": "Green", "score": 3},
+        {"text": "White", "score": 1},
+      ]
     },
     {
       'questionText': "What\s your favorite animal?",
-      "answers": ["Rabbit", "Snake", "Elephant", "Lion"]
+      "answers": [
+        {"text": "Rabbit", "score": 3},
+        {"text": "Snake", "score": 11},
+        {"text": "Elephant", "score": 5},
+        {"text": "Lion", "score": 9},
+      ]
     },
     {
       'questionText': "What\s your favorite name?",
-      "answers": ["Tony", "Tony", "Tony", "Tony"]
+      "answers": [
+        {"text": "Tony", "score": 10},
+        {"text": "Tony", "score": 5},
+        {"text": "Tony", "score": 3},
+        {"text": "Tony", "score": 1},
+      ]
     },
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
+    print('We have more questi3qwaewaewfons!');
+    setState(() {
+      //it seems you can just call setState on anything without
+      //initiaizing what a state is
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex++;
       // this triggers the re-render of new questions by continuing to the next index
@@ -57,7 +85,7 @@ class _MyAppState extends State<MyApp> {
               answerQuestion: _answerQuestion,
               questions: _questions,
               questionIndex: _questionIndex)
-          : Result(),
+          : Result(_totalScore, _resetQuiz),
     ));
   }
 }
